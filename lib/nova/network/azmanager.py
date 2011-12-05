2137e5364de05ace75226b69ffcdc5b55893572e
rpedde <ron@pedde.com>
2011-12-05 11:13:58 -0600

    Properly get availability zone from message

--- lib/nova/network/azmanager.py
+++ lib/nova/network/azmanager.py
@@ -18,13 +18,29 @@
 
 from nova import flags
 from nova.network import manager
-
+from nova import context
 
 FLAGS = flags.FLAGS
 
 
 class AZDHCPManager(manager.FlatDHCPManager):
     """Extends FlatDHCPManager to limit networks to availability zone."""
+    def init_host(self):
+        """Do any initialization that needs to be run if this is a
+        standalone service.
+        """
+        # Same as FlatDHCPManager, except only for networks with the right label.
+        # Unfortunately NetworkManager's init_host results in wrong behavior
+        # So we munge in both behaviors here, no call to super.
+        ctxt = context.get_admin_context()
+        self.driver.ensure_metadata_ip()
+        for network in self.db.network_get_all_by_host(ctxt, self.host):
+            if network['label'] == FLAGS.node_availability_zone:
+                self._setup_network(ctxt, network)
+                self.driver.init_host()
+        self.init_host_floating_ips()
+        self.driver.metadata_forward()
+
 
     def _get_networks_for_instance(self, context, instance_id, project_id,
                                    requested_networks=None):
@@ -36,5 +52,6 @@ class AZDHCPManager(manager.FlatDHCPManager):
         # NOTE(vish): expects label to be set to the name of the availability
         #             zone.  This is easier than adding a new field to the
         #             database.
+        i_ref = self.db.instance_get(context, instance_id)
         return [network for network in networks if
-                network['label'] == FLAGS.node_availability_zone]
+                network['label'] == i_ref['availability_zone']]
