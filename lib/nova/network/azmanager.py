a63fd285be4a7723f2defe1c72a214ca8018f158
rpedde <ron@pedde.com>
2011-12-05 13:12:45 -0600

    Back out host_init stuff

--- lib/nova/network/azmanager.py
+++ lib/nova/network/azmanager.py
@@ -19,29 +19,13 @@
 from nova import flags
 from nova.network import manager
 from nova import context
+from nova import log as logging
 
 FLAGS = flags.FLAGS
 
 
 class AZDHCPManager(manager.FlatDHCPManager):
     """Extends FlatDHCPManager to limit networks to availability zone."""
-    def init_host(self):
-        """Do any initialization that needs to be run if this is a
-        standalone service.
-        """
-        # Same as FlatDHCPManager, except only for networks with the right label.
-        # Unfortunately NetworkManager's init_host results in wrong behavior
-        # So we munge in both behaviors here, no call to super.
-        ctxt = context.get_admin_context()
-        self.driver.ensure_metadata_ip()
-        for network in self.db.network_get_all_by_host(ctxt, self.host):
-            if network['label'] == FLAGS.node_availability_zone:
-                self._setup_network(ctxt, network)
-                self.driver.init_host()
-        self.init_host_floating_ips()
-        self.driver.metadata_forward()
-
-
     def _get_networks_for_instance(self, context, instance_id, project_id,
                                    requested_networks=None):
         supercls = super(manager.FlatDHCPManager, self)
